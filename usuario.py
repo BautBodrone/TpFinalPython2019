016ae2a55683284138bd95a77f138894263aa13c
alejandrozt <40873520+alejandrozt@users.noreply.github.com>
2019-06-07 21:01:47 -0300

    Add files via upload

---
+++ usuario.py
@@ -0,0 +1,54 @@
+class Usuario:
+    def __init__(self, nombre, contrasena):
+        self.nombre = nombre
+        self.contrasena = contrasena
+        self.sustantivos = {}
+        self.adjetivos = {}
+        self.verbos = {}
+        self.config = {}
+
+    def setnombre(self, nombre):
+        self.nombre = nombre
+
+    def getnombre(self):
+        return self.nombre
+
+    def setcontrasena(self, contrasena):
+        self.contrasena = contrasena
+
+    def getcontrasena(self):
+        return self.contrasena
+
+    def sumarsustantivo(self, palabra, descripcion):
+        self.sustantivos[palabra] = descripcion
+
+    def getsustantivos(self):
+        return self.sustantivos
+
+    def sumaradjetivo(self, palabra, descripcion):
+        self.adjetivos[palabra] = descripcion
+
+    def getadjetivos(self):
+        return self.adjetivos
+
+    def sumarverbo(self, palabra, descripcion):
+        self.verbos[palabra] = descripcion
+
+    def getverbos(self):
+        return self.verbos
+
+    def eliminarpalabra(self, palabra):
+        if palabra in self.sustantivos.keys():
+            del self.sustantivos[palabra]
+        elif palabra in self.adjetivos.keys():
+            del self.adjetivos[palabra]
+        elif palabra in self.verbos.keys():
+            del self.verbos[palabra]
+
+    def setconfig(self, **configuracion):
+        for clave, valor in configuracion.items():
+            if valor is not None:
+                self.config[clave] = valor
+
+    def getconfig(self):
+        return self.config
