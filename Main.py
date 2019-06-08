d126360de490058a5823703a1c3908ae17abc9ac
quimeramc <51448291+quimeramc@users.noreply.github.com>
2019-06-08 04:16:55 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,50 @@
 import PySimpleGUI as sg
-import ui as config
-import tpFinal as juego
+import Configuracion as config
+import Juego as juego
+
+sg.ChangeLookAndFeel('Kayak')
 
 layout = [
-    [sg.Text("Sopa de Letras", font="arial 40")],
-    [sg.Button("Jugar", key="jugar", font="arial 20"), sg.Button("Config", key="config", font="arial 20"), sg.Button("Salir", key="salir", font="arial 20")]
+    [
+        sg.Text("Sopa de Letras", font="arial 40")
+    ],
+    [
+        sg.T('')
+    ],
+    [
+        sg.Button("Jugar", font="arial 20", key="jugar"),
+    ],
+    [
+        sg.Button("Configuracion", font="arial 20", key="config"),
+    ],
+    [
+        sg.Button("Salir", font="arial 20", key="salir")
+    ]
 ]
 
-window = sg.Window("", size=(800,600), resizable=True).Layout(layout)
+window = sg.Window(#Propiedades
+                    "Título",
+                    resizable=False,
+                                        
+                    
+                    #Diseño
+                    size=(800,600),
+                    #no_titlebar=True
+                    
+                ).Layout(layout)
+
 while True:
     event, values = window.Read()
 
-    if event == "config":
-        config.ventanaconfig()
-    if event == "jugar":
-        juego.ventanajuego()
     if event is None or event == 'salir':
         break
+
+    if event == "config":
+        window.Disappear()
+        config.abrir_configuracion()
+        window.Reappear()
+        
+    if event == "jugar":
+        window.Disappear()
+        juego.ventanajuego()
+        window.Reappear()
