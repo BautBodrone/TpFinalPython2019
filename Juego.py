a6f82eb560805da0b3df2d4c7da2b427a16792dd
quimeramc <quimeramc@users.noreply.github.com>
2019-06-24 08:48:18 -0300

    Cambios muy importantes en Juego

--- Juego.py
+++ Juego.py
@@ -7,19 +7,8 @@ import PySimpleGUI as sg
 import random
 import numpy as np
 
-#Auxiliar
-import Configuracion.Configuracion as configuracion
 
 def ventanajuego(config):  # en main juego.ventanajuego(configuracion.Configuracion.obtener_configuracion())
-    
-    def confirmar_ganador(cant_p):
-        i = 0
-        for x in range(len(cant_p)):
-            i += cant_pal[x]
-        if i == 0:
-            return True
-        else:
-            return False
 
     def cancelar_seleccion(p, correcto = False):
         for clave in p:
@@ -29,40 +18,45 @@ def ventanajuego(config):  # en main juego.ventanajuego(configuracion.Configurac
                 window.Element(clave).Update(disabled=True)
         return []
 
-    def confirmar_seleccion(p, actual, lis, cant_p, configu):
-        correcto = False
-        for i in range(len(lis)):
-            if (set(p) == set(lis[i][0]))and(actual == lis[i][2]):
-                correcto = True
-                sg.Popup("Correcto")
-                if lis[i][2] == "sustantivos":
-                    lis[i][0] = "#"  # borrado logico
-                    cant_p[0] -= 1
-                    if configu.ayudas is False:
-                        window.Element("cant_sus").Update(value="Sustantivos a encontrar: " + str(cant_p[0]))
-                elif lis[i][2] == "adjetivos":
-                    lis[i][0] = "#"
-                    cant_p[1] -= 1
-                    if configu.ayudas is False:
-                        window.Element("cant_adj").Update(value="Adjetivos a encontrar: " + str(cant_p[1]))
-                else:
-                    lis[i][0] = "#"
-                    cant_p[2] -= 1
-                    if configu.ayudas is False:
-                        window.Element("cant_verb").Update(value="Verbos a encontrar: " + str(cant_p[2]))
+    def confirmar_seleccion(p, actual, lis, cantidad_de_palabras):
+        '''Confirma si las casillas seleccionadas corresponden a una palabra en la lista de palabras'''         
+        # Lis es una lista de listas; cada lista de lis contiene:
+        # [Lista de claves, tipo] de una palabra.
+        # p es una lista de claves, actual es el tipo seleccionado.
 
+        # Por lo tanto, si se cumple la siguiente condición,
+        # podemos afirmar sin dudas que la palabra es correcta
+
+        if [p, actual] in lis:
+            correcto = True
+
+            lis.remove([p, actual])
+
+            if actual == 'sustantivos':
+                cantidad_de_palabras[0] -= 1
+            elif actual == 'adjetivos':
+                cantidad_de_palabras[1] -= 1
+            else:
+                cantidad_de_palabras[2] -= 1
+        else:
+            correcto = False
+
+        
         print(correcto)
-        return correcto
+        return cantidad_de_palabras, correcto
 
     def max_palabra(lis_palabra):
+        '''Define la cantidad de casillas de la Matriz.
+        Como mínimo será de la cantidad de palabras a colocar'''
         max = 0
-        print('lista_palabras', lis_palabra)
-        for i in lis_palabra:
-            if len(i[0])>max:
-                max = len(i[0])
-        if max<len(lis_palabra):
-            max= len(lis_palabra)
-        return max
+        for palabra in lis_palabra:
+            if len(palabra[0]) > max:
+                max = len(palabra[0])
+                
+        if max < len(lis_palabra):
+            max = len(lis_palabra)
+            
+        return max + random.randint(1, 2) # Para que la palabra más larga no quede pegada a los bordes
 
     def shuffle_pal(lista, palabras, cantidad, tipo):
         temporal = list(palabras.keys())
@@ -86,147 +80,161 @@ def ventanajuego(config):  # en main juego.ventanajuego(configuracion.Configurac
         return lista
 
     def generar_matriz(N, lis_palabra):
-        '''Genera una matriz de N filas y N columnas'''
-        solo_palabras = list(map(lambda x: x[0], lis_palabra))  # hace una lista solo con las palabras
+        '''Genera una Matriz Cuadrada de orden N'''
+
+        lista = list(map(lambda x: [x[0], x[2]], lis_palabra)) # hace una lista solo con las palabras y su tipo
+        posicionesY = sorted(random.sample(range(0, N), k=len(lista)))
+
+        # Diccionario que guarda la posición de las palabras. Clave: Línea, Valor: (Inicio, palabra)
+        posiciones = {}
+        while lista:
+            # Mientras haya palabras en la lista
+
+            #Seleccionar palabra y quitarla de la lista
+            palabra = lista[random.randrange(0, len(lista))]
+            lista.remove(palabra)
+            tipo = palabra[1]
+            palabra = palabra[0]
+
+            # Definir coordenadas para todas las palabras
+            posicionY = posicionesY.pop()
+            posicionX = random.randrange(0, (N - len(palabra))) # (X, Y)
+
+            posiciones[posicionY] = [posicionX, palabra, tipo]
+
+        # Crear la matriz
         matriz = []
-        N = N + random.randint(1, 2)  # para que la palabra mas grande no quede siempre pegada a los bordes
-        lis_pos = 0
-        orientacion = config.orientacion
-        go = range(0, N)
-        go = sorted(random.sample(go, k=len(solo_palabras)))
+        lista_claves_palabra = []
+
         for y in range(N):
             linea = []
-            entro = False
-            try:
-                if (go[0] == y) and (lis_pos < len(solo_palabras)):
-                    len_pal = len(solo_palabras[lis_pos])
-                    start = random.randrange(0, (N-len_pal))
-                    pos_agregado = 0
-                    entro = True
-                    claves = []
-                    del go[0]
-            except IndexError:
-                pass
-            for x in range(N):
-                if entro is True:
-                    if(x >= start)and(x < (start+len_pal)):
-                        claves.append(str(y) + ',' + str(x))
-                        letra = solo_palabras[lis_pos][pos_agregado]
-                        pos_agregado = pos_agregado + 1
-                    else:
-                        letra = chr(random.randint(ord('a'), ord('z')))
-                else:
-                    letra = chr(random.randint(ord('a'), ord('z')))
-                clave = str(y) + ',' + str(x)
-                linea.append(
-                    sg.Submit(  # Propiedades del botón
-                        letra,
-                        key=clave,
-                        disabled=False,
 
-                        # Diseño
-                        font='Courier 10',
-                        size=(4, 2) if N <= 12 else (2, 1),
-                        button_color=('black', 'white'),
-                        pad=(0, 0)
-                    ),
-                )
+            if y in posiciones.keys(): # La línea contiene una palabra
+                inicio = posiciones[y][0]
+                palabra = posiciones[y][1]
+                tipo = posiciones[y][2]
+                claves_palabra = []
+                posicion_letra = 0
+            else:  # La línea no contiene una palabra
+                inicio = N
+                palabra = ''
+                
+            for x in range(N):
+                if (x < inicio) or (x >= inicio + len(palabra)):
+                    letra = chr(random.randint(ord('a'), ord('z')))
+                    clave = str(y) + ',' + str(x)
+                    
+                    linea.append(
+                        sg.Submit(  # Propiedades del botón
+                            letra,
+                            key=clave,
+                            disabled=False,
+
+                            # Diseño
+                            font='Courier 10',
+                            size=(4, 2) if N <= 12 else (2, 1),
+                            button_color=('black', 'white'),
+                            pad=(0, 0)
+                        ),
+                    )
+                else: # Colocar letra de la palabra
+                    letra = palabra[posicion_letra]
+                    posicion_letra += 1
+                    clave = str(y) + ',' + str(x)
+                    claves_palabra.append(clave)
+
+                    linea.append(
+                        sg.Submit(  # Propiedades del botón
+                            letra,
+                            key=clave,
+                            disabled=False,
+
+                            # Diseño
+                            font='Courier 10',
+                            size=(4, 2) if N <= 12 else (2, 1),
+                            button_color=('black', 'white'),
+                            pad=(0, 0)
+                        ),
+                    )
             matriz.append(linea)
-            if entro is True:
-                lis_palabra[lis_pos][0] = claves  # utilizado para buscar si las palabras coiciden con lo clickeado
-                lis_pos = lis_pos + 1
-        if orientacion:  # horizontal
-            return matriz
-        else:  # vertical
-            return np.transpose(matriz)
+            if palabra != '':
+                lista_claves_palabra.append([claves_palabra, tipo])
+        
+        if not config.orientacion:  # Orientación Vertical
+            matriz = np.transpose(matriz)
+
+        return lista_claves_palabra, matriz
 
     def ayuda_frame(configu, lis_a):
+        opciones =[
+            sg.Submit(
+                'Sustantivos',
+                key='sustantivos',
+                button_color=('black', config.colores[0])
+            ),
+            sg.Submit(
+                'Adjetivos',
+                key='adjetivos',
+                button_color=('black', config.colores[1])
+            ),
+            sg.Submit(
+                'Verbos',
+                key='verbos',
+                button_color=('black', config.colores[2])
+            ),
+        ]
+
+        confirmar_cancelar = [
+            sg.Submit("PINTÉ UNA PALABRA", key="confirmar"),
+            sg.Submit('LIMPIAR LETRAS', key='cancelar')
+        ]
 
         sin_ayuda = [
-            [sg.Text(
-                "Sustantivos a encontrar: " + str(int(cant_pal[0])),
-                justification='right',
-                key="cant_sus")],
-            [sg.Text(
-                "Adjetivos a encontar: " + str(int(cant_pal[1])),
-                justification='right',
-                key="cant_adj")],
-            [sg.Text(
-                "Verbos a encontrar: " + str(int(cant_pal[2])),
-                justification='right',
-                key="cant_verb")],
             [
-                sg.Submit(
-                    'Sustantivos',
-                    key='sustantivos',
-                    button_color=('black', config.colores[0])
-                ),
-                sg.Submit(
-                    'Adjetivos',
-                    key='adjetivos',
-                    button_color=('black', config.colores[1])
-                ),
-                sg.Submit(
-                    'Verbos',
-                    key='verbos',
-                    button_color=('black', config.colores[2])
-                ),
+                sg.Text(
+                    "Sustantivos a encontrar: " + str(cantidad_de_palabras[0]),
+                    justification='right',
+                    key="cant_sus"
+                )
             ],
             [
-                sg.Submit("Confirmar", key="confirmar"), sg.Submit('CANCELAR SELECCIÓN', key='cancelar')
+                sg.Text(
+                    "Adjetivos a encontar: " + str(cantidad_de_palabras[1]),
+                    justification='right',
+                    key="cant_adj"
+                )
             ],
+            [
+                sg.Text(
+                    "Verbos a encontrar: " + str(cantidad_de_palabras[2]),
+                    justification='right',
+                    key="cant_verb"
+                )
+            ],
+            opciones,
+            confirmar_cancelar,
         ]
 
         pal_frame = [
-            [sg.Listbox(
-                values=list(map(lambda x:x[0], lis_a)),
-                size=(30, 6))],
             [
-                sg.Submit(
-                    'Sustantivos',
-                    key='sustantivos',
-                    button_color=('black', config.colores[0])
-                ),
-                sg.Submit(
-                    'Adjetivos',
-                    key='adjetivos',
-                    button_color=('black', config.colores[1])
-                ),
-                sg.Submit(
-                    'Verbos',
-                    key='verbos',
-                    button_color=('black', config.colores[2])
-                ),
+                sg.Listbox(
+                    values=list(map(lambda x:x[0], lis_a)),
+                    size=(30, 6)
+                )
             ],
-            [
-                sg.Submit("Confirmar", key="confirmar"), sg.Submit('CANCELAR SELECCIÓN', key='cancelar')
-            ],
+            opciones,
+            confirmar_cancelar,
         ]
 
         descri_frame=[
-            [sg.Listbox(
-                values=list(map(lambda x:x[1], lis_a)),
-                size=(30, 6))],
             [
-                sg.Submit(
-                    'Sustantivos',
-                    key='sustantivos',
-                    button_color=('black', config.colores[0])
-                ),
-                sg.Submit(
-                    'Adjetivos',
-                    key='adjetivos',
-                    button_color=('black', config.colores[1])
-                ),
-                sg.Submit(
-                    'Verbos',
-                    key='verbos',
-                    button_color=('black', config.colores[2])
-                ),
+                sg.Listbox(
+                    values=list(map(lambda x:x[1], lis_a)),
+                    size=(30, 6)
+                )
             ],
-            [
-                sg.Submit("Confirmar", key="confirmar"), sg.Submit('CANCELAR SELECCIÓN', key='cancelar')
-            ],
+            opciones,
+            confirmar_cancelar,
         ]
 
         if configu.ayudas is False:
@@ -237,32 +245,40 @@ def ventanajuego(config):  # en main juego.ventanajuego(configuracion.Configurac
             else:
                 return descri_frame
 
-    lis_palabras = generar_lis_palabras(config)
-    print("Lis_palabras: ", lis_palabras)
-    
-    lis_ayuda = list(map(lambda x: [x[0], x[1]], lis_palabras)) # SE PASA POR LA BOLAS QUE COPIA LA LISTA Y NO LA DIR
-    print("Lis_ayuda: ", lis_ayuda)
-    
-    num = max_palabra(lis_palabras)
-    matriz = generar_matriz(num, lis_palabras)
-    columna_izquierda = matriz
-    cant_pal = config.cantidad_de_palabras.copy()
-
-    layout = [
-        [
-            sg.Column(columna_izquierda),
-            sg.Column(ayuda_frame(config, lis_ayuda))
-        ]
-    ]
-
-    window = sg.Window('Sopa de letras').Layout(layout)
-
-
+    #Comprueba si hay suficientes palabras para la ejecución
     if (config.cantidad_de_palabras != [0.0, 0.0, 0.0]) and (len(config._lista_de_palabras) != 0):
+
+        # VARIABLES IMPORTANTES
+
+        # Matriz
+        lis_palabras = generar_lis_palabras(config)
+        N = max_palabra(lis_palabras)
+
+        # Ayudas
+        lis_ayuda = list(map(lambda x: [x[0], x[1]], lis_palabras))
+
+        # Palabras 
+        lista_claves_palabra, matriz = generar_matriz(N, lis_palabras)
+        cantidad_de_palabras = [int(n) for n in config.cantidad_de_palabras.copy()]
+
+        columna_izquierda = matriz
+        columna_derecha = ayuda_frame(config, lis_ayuda)
+        
+        layout = [
+            [
+                sg.Column(columna_izquierda),
+                sg.Column(columna_derecha)
+            ],
+        ]
+
+        window = sg.Window('Sopa de letras').Layout(layout)
+
+        # Valores iniciales para la primer ejecución
         presionadas = []
         actual = 'sustantivos'
         color = config.colores[0]
 
+        # BUCLE DE LECTURA DE LA INTERFAZ GRÁFICA DEL JUEGO
         while True:
             event, values = window.Read()
 
@@ -274,10 +290,19 @@ def ventanajuego(config):  # en main juego.ventanajuego(configuracion.Configurac
                 presionadas = cancelar_seleccion(presionadas)
 
             elif event == "confirmar":
-                confirmar = confirmar_seleccion(presionadas, actual, lis_palabras, cant_pal, config)
+                cantidad_de_palabras, confirmar = confirmar_seleccion(presionadas, actual, lista_claves_palabra, cantidad_de_palabras)
                 presionadas = cancelar_seleccion(presionadas, confirmar)
 
-                if confirmar_ganador(cant_pal):
+                if not config.ayudas:
+                    if actual == 'sustantivos':
+                        window.Element('cant_sus').Update("Sustantivos a encontrar: " + str(cantidad_de_palabras[0]))
+                    if actual == 'adjetivos':
+                        window.Element('cant_adj').Update("Adjetivos a encontrar: " + str(cantidad_de_palabras[1]))
+                    if actual == 'verbos':
+                        window.Element('cant_verb').Update("Verbos a encontrar: " + str(cantidad_de_palabras[2]))
+
+                # Ganaste
+                if not lista_claves_palabra: #Si no quedan elementos en lista_claves_palabra es que encontraste todas
                     sg.PopupOK("!!!!GANASTE!!!!")
                     window.Close()
                     break
@@ -310,7 +335,6 @@ def ventanajuego(config):  # en main juego.ventanajuego(configuracion.Configurac
     else:
         sg.PopupOK("Incremente la cantidad de palabras")
 
-
-
 if __name__ == '__main__':
+    import Configuracion.Configuracion as configuracion
     ventanajuego(configuracion.obtener_configuracion())
